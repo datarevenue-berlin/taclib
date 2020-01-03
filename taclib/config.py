98641a22f71ece4c16a583efb49162c0d1ca4c5f
pedrocwb <pedromartins.cwb@gmail.com>
2020-01-03 16:27:49 -0300

    set namespaces as env

--- taclib/config.py
+++ taclib/config.py
@@ -3,3 +3,6 @@ import confuse
 
 config = confuse.Configuration("taclib", __name__)
 config["image"] = os.environ.get("TACLIB_IMAGE", config["image"].get(str))
+config["namespaces"]["default"] = os.environ.get(
+    "K8S_NAMESPACE", config["namespaces"]["default"].get(str)
+)
