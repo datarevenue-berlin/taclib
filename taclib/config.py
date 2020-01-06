41c27f5789fa43beab319e6e2ade92fb6246f0be
pedrocwb <pedromartins.cwb@gmail.com>
2020-01-06 16:56:09 -0300

    Make variable name consistent

--- taclib/config.py
+++ taclib/config.py
@@ -4,5 +4,5 @@ import confuse
 config = confuse.Configuration("taclib", __name__)
 config["image"] = os.environ.get("TACLIB_IMAGE", config["image"].get(str))
 config["namespaces"]["default"] = os.environ.get(
-    "K8S_NAMESPACE", config["namespaces"]["default"].get(str)
+    "TACLIB_DEFAULT_NAMESPACE", config["namespaces"]["default"].get(str)
 )
